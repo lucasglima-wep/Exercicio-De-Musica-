43c8eef60a3d374d9af7bf318df3283096ca7d07
lucas gabriel <lucasglimaosusa12@gmail.com>
2025-03-13 21:27:56 -0300

    lição terminada

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿//nome da musica 
+
+Console.Clear();
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("CALOR");
+
+//letra
+
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ResetColor();
+Console.WriteLine();
+Console.WriteLine(@"Deixa o sol descer, amor
+Que eu sou fonte de calor
+Deixa a chuva cair, meu bem
+Que água nunca derreteu ninguém
+
+Se tem medo de se molhar
+Nem vem perto que eu sou cachoeira
+Tu é viela, e eu estrada
+Corrimão e eu escada
+Escorrego em você pra descer
+Pra descer
+
+Se tem medo de se molhar
+Nem vem perto que eu sou cachoeira
+Tu é viela, e eu estrada
+Corrimão e eu escada
+Escorrego em você pra descer
+Pra descer");
+
+// refrão da musica 
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine();
+Console.WriteLine(@"Deixa o sol descer, amor
+Que eu sou fonte de calor
+Deixa a chuva cair, meu bem
+Que água nunca derreteu
+Deixa o sol descer, amor
+Que eu sou fonte de calor
+Deixa a chuva cair, meu bem
+Que água nunca derreteu ninguém");
+
+//letra 
+
+Console.ResetColor();
+Console.WriteLine();
+Console.WriteLine(@"Você sempre dorme quando anoitece
+Eu fico acordada até a madrugada
+Me sinto inspirada com tudo que acontece
+E o seu dia a dia não te faz sentir nada
+
+Contigo é sempre não
+Comigo é vem, então");
+
+//refrão
+
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine(@"Deixa o sol descer, amor
+Que eu sou fonte de calor
+Deixa a chuva cair, meu bem
+Que água nunca derreteu
+Deixa o sol descer, amor
+Que eu sou fonte de calor
+Deixa a chuva cair, meu bem
+Que água nunca derreteu ninguém");
+Console.ResetColor();
+Console.Write(@"
++---------------------+
+|      OBRIGADO       |
++---------------------+");
